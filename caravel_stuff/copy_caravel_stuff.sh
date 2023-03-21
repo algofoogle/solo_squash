0593d8f554c9aa44e9bfb3825218e88cef77ab1d
Anton Maurovic <anton@foogle.com>
2023-03-21 03:11:55 +0100

    Improvement to caravel_stuff copy script

--- caravel_stuff/copy_caravel_stuff.sh
+++ caravel_stuff/copy_caravel_stuff.sh
@@ -19,9 +19,23 @@
 # This script copies the files found here in caravel_stuff/ into the respective
 # locations they need to be inside caravel_user_project.
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+ok () { printf "${GREEN}_ OK _${NC}\n"; }
+bad () { printf "${RED}_ BAD _${NC}\n"; }
+
 if [ -z "$DESIGNS" ]; then
     echo "\$DESIGNS isn't set. Normally it would point to something like:"
-    echo "~/asic_tools/caravel_user_project"
+    echo "  ~/asic_tools/caravel_user_project"
+    bad
+    exit 1
+elif [ ! -d "$DESIGNS" ]; then
+    echo "\$DESIGNS doesn't refer to a valid directory. Normally it would point to something like:"
+    echo "  ~/asic_tools/caravel_user_project"
+    echo "...but it is currently set to: '$DESIGNS'"
+    bad
     exit 1
 fi
 
@@ -30,43 +44,43 @@ export BACKUP_SUFFIX="-backup-$(date +%s)"
 backup_target () {
     if [ -f "$1" ]; then
         TT="$1$BACKUP_SUFFIX"
-        echo "$1 already exists; backing up as: $TT"
-        mv $1 $TT
+        echo -n "$1 already exists; backing up as: $TT "
+        mv $1 $TT && ok || bad
     fi
 }
 
 SS=$DESIGNS/verilog/dv/solo_squash_caravel
 mkdir -p $SS
-cp -v Makefile $SS/
-cp -v solo_squash* $SS/
-cp -v test_solo_squash_caravel.py $SS/
+cp -v Makefile $SS/ && ok || bad
+cp -v solo_squash* $SS/ && ok || bad
+cp -v test_solo_squash_caravel.py $SS/ && ok || bad
 
 T="$DESIGNS/verilog/rtl/user_project_wrapper.v"
 backup_target $T
-cp -v user_project_wrapper.v $T
+cp -v user_project_wrapper.v $T && ok || bad
 
-cp -v includes.rtl.caravel_user_project $DESIGNS/verilog/includes/
+cp -v includes.rtl.caravel_user_project $DESIGNS/verilog/includes/ && ok || bad
 
 mkdir -p $DESIGNS/openlane/solo_squash_caravel
-cp -v config.json $DESIGNS/openlane/solo_squash_caravel
+cp -v config.json $DESIGNS/openlane/solo_squash_caravel && ok || bad
 
 # klayout_gds.xml -- This can just stay in caravel_stuff for now.
 
 T="$DESIGNS/openlane/user_project_wrapper/config.json"
 backup_target $T
-cp -v UPW-config.json $T
+cp -v UPW-config.json $T && ok || bad
 
 T="$DESIGNS/openlane/user_project_wrapper/macro.cfg"
 backup_target $T
-cp -v macro.cfg $T
+cp -v macro.cfg $T && ok || bad
 
 T="$DESIGNS/README.md"
 backup_target $T
-cp -v CUP-README.md $T
+cp -v CUP-README.md $T && ok || bad
 
-T="$DESIGNS/verlog/rtl/user_defines.v"
+T="$DESIGNS/verilog/rtl/user_defines.v"
 backup_target $t
-cp -v user_defines.v $T
+cp -v user_defines.v $T && ok || bad
 
 mkdir -p $DESIGNS/docs
-cp -v docs/solo_squash_upw.png $DESIGNS/docs/
+cp -v docs/solo_squash_upw.png $DESIGNS/docs/ && ok || bad
